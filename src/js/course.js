a6be5c88d914286183a2b30f2004111f73786b27
Nick Kushkbaghi <nick.kushkbaghi@gmail.com>
2021-11-01 17:48:16 +0100

    Js update

--- src/js/course.js
+++ src/js/course.js
@@ -1,32 +1,32 @@
 
 
-// // Create variable
-// var studier_table= document.getElementById("courses_table");
+// Create variable
+var studier_table= document.getElementById("courses_table");
 
 
-// // louad the page
-// window.addEventListener("load", getCourses);
+// louad the page
+window.addEventListener("load", getCourses);
 
 
 
 
-// // contact to API and print JSON container
-// function getCourses() {
-//     studier_table.innerHTML = "";
+// contact to API and print JSON container
+function getCourses() {
+    studier_table.innerHTML = "";
 
-//     fetch('https://studenter.miun.se/~niku2001/writeable/webb3/api/api.php?table=courses')
-//         .then(response => response.json())
-//         .then(data => {
+    fetch('https://studenter.miun.se/~niku2001/writeable/webb3/api/api.php?table=courses')
+        .then(response => response.json())
+        .then(data => {
 
-//             data.forEach((item) => {
-//                 studier_table.innerHTML += `<tr>
-//                     <td>${item.course}</td>
-//                     <td>${item.place}</td>
-//                     <td>${item.start_date}</td>
-//                     <td> ${item.end_date}</td>
+            data.forEach((item) => {
+                studier_table.innerHTML += `<tr>
+                    <td>${item.course}</td>
+                    <td>${item.place}</td>
+                    <td>${item.start_date}</td>
+                    <td> ${item.end_date}</td>
                    
-//                 </tr>`
-//             })
-//         })
-// }
+                </tr>`
+            })
+        })
+}
 
--- src/js/work.js
+++ src/js/work.js
@@ -1,28 +1,28 @@
 
-// // Create variable
-// var works_table= document.getElementById("works_table");
+// Create variable
+var works_table= document.getElementById("works_table");
 
 
 
-// // louad the page
-// window.addEventListener("load", getWorks);
+// louad the page
+window.addEventListener("load", getWorks);
 
-// // contact to API and print JSON container
-// function getWorks() {
-//     works_table.innerHTML = "";
+// contact to API and print JSON container
+function getWorks() {
+    works_table.innerHTML = "";
 
-//     fetch('https://studenter.miun.se/~niku2001/writeable/webb3/api/api.php?table=works')
-//         .then(response => response.json())
-//         .then(data => {
+    fetch('https://studenter.miun.se/~niku2001/writeable/webb3/api/api.php?table=works')
+        .then(response => response.json())
+        .then(data => {
 
-//             data.forEach((item) => {
-//                 works_table.innerHTML += `<tr>
-//                     <td>${item.work}</td>
-//                     <td>${item.place}</td>
-//                     <td>${item.start_date}</td>
-//                     <td> ${item.end_date}</td>
+            data.forEach((item) => {
+                works_table.innerHTML += `<tr>
+                    <td>${item.work}</td>
+                    <td>${item.place}</td>
+                    <td>${item.start_date}</td>
+                    <td> ${item.end_date}</td>
                    
-//                 </tr>`
-//             })
-//         })
-// }
+                </tr>`
+            })
+        })
+}
